de541afaaad45224083fdacb5e6b1ace72c92c17
thanhtd <thanhtd@sabitech.vn>
2026-03-16 16:06:42 +0700

    finish js-info-excercise for function

--- javascript.js
+++ javascript.js
@@ -51,18 +51,95 @@ console.log(camelize(testString3));
 // alert(arr); // 5,3,8,1 (not modified)
 
 //////////////////////
-function filterRangeInPlace(arr, a, b) {
-  const condition = (item) => item >= a && item <= b;
+// function filterRangeInPlace(arr, a, b) {
+//   const condition = (item) => item >= a && item <= b;
 
-  for (let i = arr.length - 1; i >= 0; i--) {
-    if (!condition(arr[i])) {
-      arr.splice(i, 1);
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     if (!condition(arr[i])) {
+//       arr.splice(i, 1);
+//     }
+//   }
+// }
+
+// let arr = [5, 3, 8, 1];
+
+// filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+
+// alert(arr); // [3, 1]
+
+// //////////////////////
+
+// const points = [40, 100, 1, 5, 25, 10];
+// console.log(points.sort((a, b) => a - b));
+
+/////////////////////
+
+// let arr = ["HTML", "JavaScript", "CSS"];
+
+// let sorted = copySorted(arr);
+
+// alert(sorted); // CSS, HTML, JavaScript
+// alert(arr); // HTML, JavaScript, CSS (no changes)
+
+// function copySorted(arr) {
+//   const copyArr = arr.slice();
+//   copyArr.sort((a, b) => a.localeCompare(b));
+//   return copyArr;
+// }
+
+/////////////////////////////////
+
+// arr = [1, 2, 45, 234, 6, 1];
+// let randomeValue = Math.floor(Math.random() * arr.length);
+
+// let arr = [1, 2, 3];
+// shuffle(arr);
+// // arr = [3, 2, 1]
+// shuffle(arr);
+// // arr = [2, 1, 3]
+// shuffle(arr);
+// // arr = [3, 1, 2]
+// // ...
+// console.log(shuffle(arr));
+// console.log(shuffle(arr));
+// console.log(shuffle(arr));
+
+// function shuffle(arr) {
+//   let valueToSwitch;
+//   let randomeValue;
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     randomeValue = Math.floor(Math.random() * (i + 1));
+//     let tempValue = arr[i];
+//     valueToSwitch = arr[randomeValue];
+//     arr[i] = valueToSwitch;
+//     arr[randomeValue] = tempValue;
+//   }
+//   return arr;
+// }
+
+/////////////////////////////////
+
+function unique(arr) {
+  let newArr = [];
+  for (let i = 0; i <= arr.length - 1; i++) {
+    if (!newArr.includes(arr[i])) {
+      let value = arr[i];
+      newArr.push(value);
     }
   }
+  return newArr;
 }
 
-let arr = [5, 3, 8, 1];
+let strings = [
+  "Hare",
+  "Krishna",
+  "Hare",
+  "Krishna",
+  "Krishna",
+  "Krishna",
+  "Hare",
+  "Hare",
+  ":-O",
+];
 
-filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
-
-alert(arr); // [3, 1]
+alert(unique(strings)); // Hare, Krishna, :-O
